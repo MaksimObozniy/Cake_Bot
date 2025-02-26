ed6777e8e764505ee84bb0420775defde275b666
MaksimObozniy <mobozn@gmail.com>
2025-02-26 21:58:37 +0400

    add description for field

--- property/models.py
+++ property/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Cake_levels(models.Model):
     levels = models.CharField(
+        "Кол-во уровней торта",
         max_length=20,
         null=False,
         blank=False
@@ -14,6 +15,7 @@ class Cake_levels(models.Model):
 
 class Cake_Shape(models.Model):
     name = models.CharField(
+        "Форма торта",
         max_length=20,
         null=False,
         blank=False
@@ -22,6 +24,7 @@ class Cake_Shape(models.Model):
 
 class Cake_Topping(models.Model):
     name = models.CharField(
+        "Топпинг торта"
         max_length=30,
         null=False,
         blank=False
@@ -30,6 +33,7 @@ class Cake_Topping(models.Model):
 
 class Cake_Berries(models.Model):
     name = models.CharField(
+        "Ягоды для торта",
         max_length=50,
         blank=False,
         null=False
@@ -38,6 +42,7 @@ class Cake_Berries(models.Model):
 
 class Cake_Decor(models.Model):
     name = models.CharField(
+        "Декор для торта",
         max_length=50,
         blank=False,
         null=False
@@ -46,10 +51,10 @@ class Cake_Decor(models.Model):
 
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
-    adress = models.CharField(max_length=100)
-    date = models.DateTimeField()
-    comment = models.TextField(max_length=1000, null=True)
-    title = models.CharField(max_length=50)
+    adress = models.CharField("Адресс доставки", max_length=100)
+    date = models.DateTimeField("Дата доставки", null=False)
+    comment = models.TextField("Комментарий к заказу", max_length=1000, null=True)
+    title = models.CharField("Надпись на торте", max_length=50, null=True)
     
     level = models.ForeignKey(Cake_levels, on_delete=models.CASCADE)
     shape = models.ForeignKey(Cake_Shape, on_delete=models.CASCADE)
@@ -58,5 +63,12 @@ class Order(models.Model):
     
     
 class User (models.Model):
-    tg_id = models.CharField(max_length=50)
-    orders = models.ForeignKey(Order, on_delete=models.CASCADE)
+    first_name = models.CharField("Имя клиента", max_length=20)
+    phone_number = models.CharField(
+        "Номер владельца",
+        max_length=20,
+        db_index=True
+        )
+    
+    tg_id = models.CharField("tg id пользователя", max_length=50)
+    orders = models.ForeignKey(Order, on_delete=models.CASCADE, verbose_name="История заказов")
